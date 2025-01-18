54286f912e7ef9251fb50b9db58f455fe25a547b
Deejs2 <jireldhiraj123@gmail.com>
2025-01-18 23:48:09 +0545

    bug fix of navbar

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
